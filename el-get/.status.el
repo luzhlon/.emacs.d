f07dc20873208838e1045e9e664b1af6583183be
luzhlon <luzhlon@126.com>
2015-08-09 14:03:46 +0800

    initial

---
+++ el-get/.status.el
@@ -0,0 +1,66 @@
+((auto-complete status "installed" recipe
+                (:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+                       (popup fuzzy)
+                       :features auto-complete-config :post-init
+                       (progn
+                         (add-to-list 'ac-dictionary-directories
+                                      (expand-file-name "dict" default-directory))
+                         (ac-config-default))))
+ (auto-complete-clang status "installed" recipe
+                      (:name auto-complete-clang :website "https://github.com/brianjcj/auto-complete-clang" :description "Auto-complete sources for Clang. Combine the power of AC, Clang and Yasnippet." :type github :pkgname "brianjcj/auto-complete-clang" :depends auto-complete))
+ (cl-lib status "installed" recipe
+         (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (color-theme status "installed" recipe
+              (:name color-theme :description "An Emacs-Lisp package with more than 50 color themes for your use. For questions about color-theme" :website "http://www.nongnu.org/color-theme/" :type http-tar :options
+                     ("xzf")
+                     :url "http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz" :load "color-theme.el" :features "color-theme" :post-init
+                     (progn
+                       (color-theme-initialize)
+                       (setq color-theme-is-global t))))
+ (color-theme-almost-monokai status "installed" recipe
+                             (:name color-theme-almost-monokai :description "A beautiful, fruity, calm, yet dark color theme for Emacs." :type github :pkgname "lut4rp/almost-monokai" :depends color-theme :prepare
+                                    (autoload 'color-theme-almost-monokai "color-theme-almost-monokai" "color-theme: almost-monokai" t)))
+ (el-get status "installed" recipe
+         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "master" :pkgname "dimitri/el-get" :info "." :compile
+                ("el-get.*\\.el$" "methods/")
+                :features el-get :post-init
+                (when
+                    (memq 'el-get
+                          (bound-and-true-p package-activated-list))
+                  (message "Deleting melpa bootstrap el-get")
+                  (unless package--initialized
+                    (package-initialize t))
+                  (when
+                      (package-installed-p 'el-get)
+                    (let
+                        ((feats
+                          (delete-dups
+                           (el-get-package-features
+                            (el-get-elpa-package-directory 'el-get)))))
+                      (el-get-elpa-delete-package 'el-get)
+                      (dolist
+                          (feat feats)
+                        (unload-feature feat t))))
+                  (require 'el-get))))
+ (evil status "installed" recipe
+       (:name evil :website "https://bitbucket.org/lyro/evil" :description "Evil is an extensible vi layer for Emacs. It\n       emulates the main features of Vim, and provides facilities\n       for writing custom extensions." :type hg :url "https://bitbucket.org/lyro/evil" :features evil :depends
+              (undo-tree goto-chg)
+              :build
+              (("make" "info" "all"))
+              :build/berkeley-unix
+              (("gmake" "info" "all"))
+              :build/darwin
+              `(("make" ,(format "EMACS=%s" el-get-emacs)
+                 "info" "all"))
+              :info "doc"))
+ (fuzzy status "installed" recipe
+        (:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (goto-chg status "installed" recipe
+           (:name goto-chg :description "Goto the point of the most recent edit in the buffer." :type emacswiki :features goto-chg))
+ (popup status "installed" recipe
+        (:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :submodule nil :depends cl-lib :pkgname "auto-complete/popup-el"))
+ (undo-tree status "installed" recipe
+            (:name undo-tree :description "Treat undo history as a tree" :website "http://www.dr-qubit.org/emacs.php" :type git :url "http://www.dr-qubit.org/git/undo-tree.git/"))
+ (yasnippet status "installed" recipe
+            (:name yasnippet :website "https://github.com/capitaomorte/yasnippet.git" :description "YASnippet is a template system for Emacs." :type github :pkgname "capitaomorte/yasnippet" :compile "yasnippet.el" :submodule nil :build
+                   (("git" "submodule" "update" "--init" "--" "snippets")))))
